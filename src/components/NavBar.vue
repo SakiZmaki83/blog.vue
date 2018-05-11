5897eb225fb42992675a81ec44cf1063b5021576
jelena <jelena>
2018-05-11 18:50:12 +0200

    zadatak1

---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+      <router-link class="navbar-brand" to="posts">PostsApp</router-link>
+
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <router-link class="nav-link" to="posts">Posts</router-link>
+          </li>
+         
+        </ul>
+      </div>
+    </nav>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
---
+++ src/pages/Posts.vue
@@ -0,0 +1,17 @@
+<template>
+<div>
+    <h1>Posts</h1>        
+</div>
+</template>
+
+<script>
+
+
+export default {
+    components: {
+       
+    }
+   
+}
+
+</script>
